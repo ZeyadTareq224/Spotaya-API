9c287aca5241031fc4a3fc494d01d56c5775b99d
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-05-05 18:48:44 +0300

    added an endpoint to get the name and the location of near by places

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
+
+
+@app.route("/nearby-places", methods=["GET"])
+def get_nearby_places():
+    """
+    Send a request to google places api to get the nearby places of intrest to the user
+
+    """
+
+    API_KEY = "AIzaSyDuZzGxZqFea9knHTxDpxkJQsdcxY0T2V0"
+    URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    payload = {
+        "key": API_KEY,
+        "location": f"{request.args.get('lng')}, {request.args.get('lat')}",
+        "radius": "1000",
+        "keyword": request.args.get("type"),
+    }
+    results = requests.get(URL, params=payload)
+    if results.status_code == 200:
+        response = []
+        places_data = results.json()
+        for place in places_data["results"]:
+            response_dict = {}
+            response_dict["name"] = place["name"]
+            response_dict["location"] = place["geometry"]["location"]
+            response.append(response_dict)
+    return jsonify(response)
