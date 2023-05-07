183d414edb7ef1bf3edbe05df59af24045af61e6
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-05-07 22:12:21 +0300

    added activity time

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import requests
 import os
 import random
+from datetime import datetime
 
 load_dotenv()
 
@@ -11,6 +12,16 @@ app = Flask(__name__)
 
 def get_random_item(mylist):
     return random.choice(mylist)
+
+
+def calculate_time_delta(from_time, to_time, activities_no):
+    from_time_obj = datetime.strptime(from_time, "%H:%M:%S").time()
+    to_time_obj = datetime.strptime(to_time, "%H:%M:%S").time()
+    delta = datetime.combine(datetime.min, to_time_obj) - datetime.combine(
+        datetime.min, from_time_obj
+    )
+    time_diff = delta / activities_no
+    return str(time_diff)
 
 
 @app.route("/nearby-places", methods=["GET"])
@@ -22,6 +33,8 @@ def get_nearby_places():
     API_KEY = os.getenv("GOOGLE_API_KEY")
     URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     places_types = request.args.get("types").split("|")
+    from_time = request.args.get("from_time")
+    to_time = request.args.get("to_time")
     response = []
 
     for place_type in places_types:
@@ -41,6 +54,9 @@ def get_nearby_places():
                 "name": random_place["name"],
                 "location": random_place["geometry"]["location"],
                 "types": random_place["types"],
+                "activity_duration": calculate_time_delta(
+                    from_time, to_time, len(places_types)
+                ),
             }
             response_dict[place_type] = simple_place_data
             response.append(response_dict)
